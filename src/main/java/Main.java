d97c6a06a720bb6b03a2c0312e576d4416fe3379
MyGuyKai <kaiw8t@gmail.com>
2022-11-22 01:34:49 -0700

    Docker update

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String [] args) {
+    boolean shouldContinue = true;
+    Calculator calculator = new Calculator();
+
+    System.out.println("Enter a command (q to quit):");
+    while(shouldContinue) {
+      Scanner scanner = new Scanner(System.in);
+      String command = scanner.nextLine();
+      String [] input = command.split(" ");
+
+      switch(input[0]) {
+        case "add":
+          System.out.println(calculator.add(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+          break;
+        case "subtract":
+          System.out.println(calculator.subtract(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+          break;
+        case "multiply":
+          System.out.println(calculator.multiply(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+          break;
+        case "divide":
+          System.out.println(calculator.divide(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+          break;
+        case "fibonacci":
+          System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt( input[1])));
+          break;
+        case "binary":
+          System.out.println(calculator.intToBinaryNumber(Integer.parseInt(input[1])));
+          break;
+        case "q":
+          shouldContinue = false;
+          break;
+        default:
+          System.out.println("Unrecognized command");
+      }
+    }
+  }
+}
